a41f4b3531db3c795aba4b76502c0e06fba1a721
Oleksandra <predictormagnolia@gmail.com>
2023-04-18 14:12:19 +0300

    Add project files.

---
+++ ConsoleApp1/Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Bank
+    {
+        private int Money;
+        public string Name { get; set; }
+        private int Percent;
+        public int _Money
+        {
+            get => Money;
+            set
+            {
+                Money = value;
+                Thread th = new Thread(new ThreadStart(PropertyChanged));
+                th.IsBackground = true;
+            }
+        }
+        public int _Percent
+        {
+            get => Percent;
+            set
+            {
+                Percent = value;
+                Thread th = new Thread(new ThreadStart(PropertyChanged));
+                th.IsBackground = true;
+            }
+        }
+        public void PropertyChanged()
+        {
+            Thread th = Thread.CurrentThread;
+            StreamWriter writer = new StreamWriter("Bank.txt", true);
+            writer.WriteLine("Money: " + Money);
+            writer.WriteLine("Name: " + Name);
+            writer.WriteLine("Percent: " + Percent);
+            writer.Close();
+            th.Abort();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void CollectionToStringThread(object obj)
+        {
+            List<int> list = (List<int>)obj;
+            Thread t = Thread.CurrentThread;
+            foreach(int item in list)
+            {
+                Console.WriteLine(item.ToString());
+               Thread.Sleep(100);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Thread th = new Thread(new ParameterizedThreadStart(CollectionToStringThread));
+            th.IsBackground = true;
+            th.Start(list);
+        }
+    }
+
+}
